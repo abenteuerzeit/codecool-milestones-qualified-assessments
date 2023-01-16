9b9bdae2800b86e09fc42fd5a4ee474ed56b8da1
abenteuerzeit <amroz.muzyka@gmail.com>
2023-01-16 07:10:13 +0100

    use linq

--- Fullstack-Milestone-7-C-VariationA1/StringToDict/StringToDict.cs
+++ Fullstack-Milestone-7-C-VariationA1/StringToDict/StringToDict.cs
@@ -1,58 +1,19 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace CSharpMilestone
 {
-    public static class StringToDictionaryChallenge
+    public static class ItemFunctions
     {
-        public static Dictionary<string, int> StringToDictionary(string s)
-        {
-            var items = s.Split(';');
-            var itemDictionary = new Dictionary<string, int>();
+        public static Dictionary<string, int> StringToDictionary(string s) => 
+            s.Split(';')
+            .GroupBy(i => i)
+            .ToDictionary(g => g.Key, g => g.Count());
 
-            foreach (var item in items)
-            {
-                if (itemDictionary.ContainsKey(item))
-                {
-                    itemDictionary[item]++;
-                }
-                else
-                {
-                    itemDictionary.Add(item, 1);
-                }
-            }
-            return itemDictionary;
-        }
+        public static string GetItemWithGreatestAmount(Dictionary<string, int> itemDictionary) =>
+            itemDictionary.Count == 0 ? "" : itemDictionary.OrderByDescending(i => i.Value).First().Key;
 
-        public static string GetItemWithGreatestAmount(Dictionary<string, int> itemDictionary)
-        {
-            if (itemDictionary.Count == 0)
-            {
-                return "";
-            }
-            var maxItem = itemDictionary.First();
-            foreach (var item in itemDictionary)
-            {
-                if (item.Value > maxItem.Value)
-                {
-                    maxItem = item;
-                }
-            }
-            return maxItem.Key;
-        }
-
-        public static List<string> GetItemsWithAmountLesserThan(Dictionary<string, int> itemDictionary, int amount)
-        {
-            var result = new List<string>();
-            foreach (var item in itemDictionary)
-            {
-                if (item.Value < amount)
-                {
-                    result.Add(item.Key);
-                }
-            }
-            return result;
-        }
+        public static List<string> GetItemsWithAmountLesserThan(Dictionary<string, int> itemDictionary, int amount) =>
+            itemDictionary.Where(i => i.Value < amount).Select(i => i.Key).ToList();
     }
 }
